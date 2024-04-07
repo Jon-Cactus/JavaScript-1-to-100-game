0cdfa19ca01117f3d9e49fd9d3d0f82eec23978d
Jon-Cactus <157452914+Jon-Cactus@users.noreply.github.com>
2024-04-07 22:44:51 +0900

    Initial Upload
    Still some issues, and needs styling.

---
+++ Project_JavaScript-1-to-100-game/c3vanBt8/index.js
@@ -0,0 +1,64 @@
+const guessBtn = document.getElementById("guess-btn")
+const newGameBtn = document.getElementById("new-game-btn")
+const guessesEl = document.getElementById("guesses-el")
+const checkEl = document.getElementById("check-el")
+const hintEl = document.getElementById("hint-el")
+const guessNumber = document.getElementById("guess-number")
+let correctNumber = Math.floor(Math.random() * 100) + 1;
+console.log(correctNumber)
+let round = 0
+
+//Current issues://
+//When starting a new game, the correctNumber remains the same despite trying to reset it with the newGameBtn's function//
+
+guessBtn.addEventListener("click", function() {
+    if (guessNumber.value == correctNumber) {
+        guessesEl.textContent += guessNumber.value + ", ";
+        checkEl.removeAttribute("hidden");
+        checkEl.style.backgroundColor = "green";
+        checkEl.textContent = "Congratulations! You got it right!";
+        guessBtn.setAttribute("disabled", "");
+        newGameBtn.removeAttribute("hidden");
+    } else if (guessNumber.value > correctNumber) {
+        guessesEl.textContent += guessNumber.value + ", ";
+        checkEl.removeAttribute("hidden");
+        checkEl.textContent = "Wrong!";
+        hintEl.removeAttribute("hidden");
+        hintEl.textContent = "Last guess was too high!";
+        round++; {
+            if (round === 10) {
+                checkEl.textContent = "!!!GAME OVER!!!";
+                newGameBtn.removeAttribute("hidden");
+                guessBtn.setAttribute("disabled", "");
+            }
+        }
+    } else if (guessNumber.value < correctNumber) {
+        guessesEl.textContent += guessNumber.value + ", ";
+        checkEl.removeAttribute("hidden");
+        checkEl.textContent = "Wrong!";
+        hintEl.removeAttribute("hidden");
+        hintEl.textContent = "Last guess was too low!";
+        round++; {
+            if (round === 10) {
+                checkEl.textContent = "!!!GAME OVER!!!";
+                newGameBtn.removeAttribute("hidden");
+                guessBtn.setAttribute("disabled", "");
+            }
+        }
+    }
+})
+
+newGameBtn.addEventListener("click", function() {
+    guessNumber.value = "null"
+    round = 0;
+    guessesEl.textContent = "";
+    checkEl.textContent = "";
+    hintEl.textContent = "";
+    guessesEl.setAttribute("hidden", "");
+    checkEl.setAttribute("hidden", "")
+    hintEl.setAttribute("hidden", "");
+    newGameBtn.setAttribute("hidden", "");
+    guessBtn.removeAttribute("disabled")
+    //Resetting the correctNumber doesn't seem to work this way//
+    let correctNumber = Math.floor(Math.random() * 100) + 1;
+})
